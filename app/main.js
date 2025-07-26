0baa4545a5d6d2cc9ffdda2abfdd30fbcf26ec5d
yuvansh jain <yuvanshjain95@gmail.com>
2025-07-25 23:39:51 -0400

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,15 +1,14 @@
 const net = require("net");
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-//   // Handle connection
-connection.on("data", (data) => {
+  // Handle connection
+  // Handle connection
+  connection.on("data", (data) => {
     connection.write("+PONG\r\n");
-    connection.end();
+    console.log(data.toString().split("\n"));
+  });
+  connection.on("end", () => {
+    console.log("client disconnected");
+  });
 });
-});
-//
 server.listen(6379, "127.0.0.1");
